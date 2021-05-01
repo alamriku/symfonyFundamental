958185e2555f9335f58b1ff78d153acb5edcb5b6
Riku <alamriku069@gmail.com>
2021-05-02 00:49:04 +0600

    most the part are theory about Symfony Services Object see document.Twig,Connection.Logger,HttpClient,Cache,Router

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,12 +18,14 @@ class CommentController extends AbstractController
      * @Route("/comments/{id}/vote/{direction<up|down>}", methods="POST") smart routes
      */
 
-    public function commentVote($id, $direction)
+    public function commentVote($id, $direction, LoggerInterface $logger)
     {
         //user real login here to save this to the database
         if($direction === "up"){
+            $logger->info('Voting up');
             $currentVoteCount = rand(7, 100);
         } else {
+            $logger->info('Voting down');
             $currentVoteCount = rand(0, 5);
         }
 
