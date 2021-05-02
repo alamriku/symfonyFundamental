0bc0c28f02e221bf50c7cb5187a9176795294b99
Riku <alamriku069@gmail.com>
2021-05-02 17:28:01 +0600

    markdown plugin knpmarkdown is install and used

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -25,13 +26,18 @@ class QuestionController extends AbstractController
     /**
      * @Route("questions/{slug}",name="app_question_show")
      */
-    public function show($slug)
+    public function show($slug, MarkdownParserInterface $markdownParser)
     {
-        $ans = ['fak','fak'];
-
+        $answers = [
+            '`Markdown` aliquet quam id dui posuere blandit. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.',
+            'Donec rutrum congue leo eget malesuada.'
+        ];
+        $questionText = "I've been turned into a cat, any thoughts on how to turn back? While I'm **adorable**, I don't really care for cat food.";
+        $parsedQuestion = $markdownParser->transformMarkdown($questionText);
         return $this->render('question/show.html.twig',[
             'question' => ucwords(str_replace('-',' ', $slug)),
-            'answers' => ['fak','fak']
+            'questionText' => $parsedQuestion,
+            'answers' => $answers
         ]);
         //return new Response(sprintf('anotation test "%s"', ucwords(str_replace('-',' ', $slug))));
     }
