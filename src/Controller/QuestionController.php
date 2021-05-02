5296d99382b2041a869febb31e9e8e6f3700dfe5
Riku <alamriku069@gmail.com>
2021-05-02 18:12:47 +0600

    cache service is used using symfony concrete cache interface

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -8,6 +8,7 @@ use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
 use Twig\Environment;
 
 class QuestionController extends AbstractController
@@ -26,14 +27,16 @@ class QuestionController extends AbstractController
     /**
      * @Route("questions/{slug}",name="app_question_show")
      */
-    public function show($slug, MarkdownParserInterface $markdownParser)
+    public function show($slug, MarkdownParserInterface $markdownParser, CacheInterface $cache)
     {
         $answers = [
-            '`Markdown` aliquet quam id dui posuere blandit. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.',
+            '`Markdown` aliquet **Second markdown with cache** id dui posuere blandit. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.',
             'Donec rutrum congue leo eget malesuada.'
         ];
-        $questionText = "I've been turned into a cat, any thoughts on how to turn back? While I'm **adorable**, I don't really care for cat food.";
-        $parsedQuestion = $markdownParser->transformMarkdown($questionText);
+        $questionText = "I've `been` turned into a cat, any thoughts on how to turn back? While I'm **adorable**, I don't really care for cat food.";
+        $parsedQuestion = $cache->get('markdown_'.md5($questionText), function () use ($markdownParser,$questionText){
+            return $markdownParser->transformMarkdown($questionText);
+        });
         return $this->render('question/show.html.twig',[
             'question' => ucwords(str_replace('-',' ', $slug)),
             'questionText' => $parsedQuestion,
